656bbb9657a01cd497263ee92de3ca6f8bf22e2f
Bruce Van Allen <bva@cruzio.com>
2024-05-19 14:58:30 -0700

    Major Update to spinnerComponent.js
    Added: constructor accepts optiopns for attributes of the spinner.
    Added: setAttributes method, used by the constructor and also available to spinner instances.
    Expanded list of observedAttributes, both to add more custom attributes for the spinners, and also to include style, name, and id. The list of observedAttributes restricts the attributes that maybe applied in markup and by using setAttributes in the constructor method, also restricts which atributes may be set in "new SpinnerElement".
    Simplified the attributeChangedCallback method's syntax.
    Added a "back" feature to spinners, providing some padded background color around the spinner and any prefix or suffix.
    Moved the specific css for using the border sides as the spinning part to a separate template.
    Changed element idenification from based on id to any of id, name, tag, class, by using querySelector;
    Added a versatile removeSpinner method, along side insertSpinner and appendSpinner.

--- src/spinnerComponent.js
+++ src/spinnerComponent.js
@@ -1,55 +1,81 @@
 class SpinnerElement extends HTMLElement {
-  constructor() {
+  constructor( options = {} ) {
     super();
     this.attachShadow({ mode: 'open' });
+    this.setAttributes(options);
   }
 
   static get observedAttributes() {
     return [
       'color', 'clr', 'speed', 'sp', 'direction', 'dir',
+      'background-color', 'bgclr',
       'trace-color', 'tclr', 'cursor', 'crsr',
       'prefix', 'pre', 'suffix', 'suf', 'kerning', 'kern',
-      'back-color', 'bclr', 'weight', 'wt'
+      'back-color', 'bkclr', 'weight', 'wt',
+      'cursor-style', 'cstyle', 'style', 'name', 'id'
     ];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
-    if (
-         name === 'color'
-      || name === 'clr'
-      || name === 'speed'
-      || name === 'sp'
-      || name === 'direction'
-      || name === 'dir'
-      || name === 'trace-color'
-      || name === 'tclr'
-      || name === 'cursor'
-      || name === 'crsr'
-      || name === 'prefix'
-      || name === 'pre'
-      || name === 'suffix'
-      || name === 'suf'
-      || name === 'kerning'
-      || name === 'kern'
-      || name === 'back-color'
-      || name === 'bclr'
-      || name === 'weight'
-      || name === 'wt'
-    ) {
-      this.render();
+    if (this.constructor.observedAttributes.includes(name)) {
+    this.render();
     }
+  }
+
+  setAttributes(options = {}) {
+   const observedAttributes = this.constructor.observedAttributes;
+   Object.entries(options).forEach(([key, value]) => {
+     if (observedAttributes.includes(key)) {
+       this.setAttribute(key, value);
+     } else {
+       console.warn(`Ignoring unknown attribute "${key}" for <x-spinner>`);
+     }
+    });
+  }
+
+  setStyle(style = {}) {
+    Object.entries(style).forEach(([property, value]) => {
+      this.style.setProperty(property, value);
+    });
+  }
+
+  setSolidCursor() {
+    this.setAttribute('cursor-style', 'solid');
+  }
+  setDottedCursor() {
+    this.setAttribute('cursor-style', 'dotted');
+  }
+  setDoubleCursor() {
+    this.setAttribute('cursor-style', 'double');
+  }
+  setDashedCursor() {
+    this.setAttribute('cursor-style', 'dashed');
+  }
+  setGrooveCursor() {
+    this.setAttribute('cursor-style', 'groove');
+  }
+  setRidgeCursor() {
+    this.setAttribute('cursor-style', 'ridge');
+  }
+  setInsetCursor() {
+    this.setAttribute('cursor-style', 'inset');
+  }
+  setOutsetCursor() {
+    this.setAttribute('cursor-style', 'outset');
   }
 
   render() {
     const crsrcolor  = this.getAttribute('color')       || this.getAttribute('clr')   || 'currentColor';
     const tracecolor = this.getAttribute('trace-color') || this.getAttribute('tclr')  || 'rgba(20, 20, 20, .1)';
+    const bkcolor    = this.getAttribute('back-color')  || this.getAttribute('bkclr')  || '';
+    const bgcolor    = this.getAttribute('background-color') || this.getAttribute('bgclr')  || 'transparent';
+
     const speed      = this.getAttribute('speed')       || this.getAttribute('sp')    || '1';
-    const direction  = this.getAttribute('direction')   || this.getAttribute('dir')   || 'cw';
     const kerning    = this.getAttribute('kerning')     || this.getAttribute('kern')  || '0';
     const prefix     = this.getAttribute('prefix')      || this.getAttribute('pre')   || '';
     const suffix     = this.getAttribute('suffix')      || this.getAttribute('suf')   || '';
-    const bgcolor    = this.getAttribute('back-color')  || this.getAttribute('bclr')  || '';
-    const cursor     = this.getAttribute('cursor')      || this.getAttribute('crsr')  || '1000';
+    const cstyle     = this.getAttribute('cursor-style')|| this.getAttribute('cstyle')|| 'solid';
+
     let weight       = this.getAttribute('weight')      || this.getAttribute('wt')    || '0.195';
     switch (true) {
       case (weight <= .5 && weight > 0):
@@ -62,25 +88,46 @@ class SpinnerElement extends HTMLElement {
         break;
     }
 
+    const direction  = this.getAttribute('direction')   || this.getAttribute('dir')   || 'cw';
+    const animation  = direction === 'cw' ? 'spinner' : 'spinner_rev';
+
+    const cursor       = this.getAttribute('cursor')      || this.getAttribute('crsr')  || '1000';
     const crsr_pat     = cursor.split('');
     const top_color    = crsr_pat[0] == 1 ? crsrcolor : tracecolor;
     const left_color   = crsr_pat[1] == 1 ? crsrcolor : tracecolor;
     const bottom_color = crsr_pat[2] == 1 ? crsrcolor : tracecolor;
     const right_color  = crsr_pat[3] == 1 ? crsrcolor : tracecolor;
 
-    let back_style;
-    if (bgcolor) {
-      back_style = `background-color: ${bgcolor};
-          padding: .191em .38em .191em .38em;
-          border-radius: .33em;
-          color: ${crsrcolor};`
-    }
-    else {
-      back_style = `background-color: transparent;
-          color: inherit;`
-    }
+    const backStyle = bkcolor ?
+      `background-color: ${bkcolor};
+         padding: .191em .38em .191em .38em;
+         border-radius: .33em;
+         color: ${crsrcolor};`
+    : `background-color: transparent;
+         color: inherit;`;
 
-    const animation = direction === 'cw' ? 'spinner' : 'spinner_rev';
+    const circleBorderSpinnerCss  = `
+       border-radius: 50%;
+       @supports (width: 1cap) {
+         width:         1cap;
+         height:        1cap;
+         border:        ${weight}cap ${cstyle} ${tracecolor};
+         border-top:    ${weight}cap ${cstyle} ${top_color};
+         border-bottom: ${weight}cap ${cstyle} ${bottom_color};
+         border-left:   ${weight}cap ${cstyle} ${left_color};
+         border-right:  ${weight}cap ${cstyle} ${right_color};
+       }
+       @supports not (width: 1cap) {
+         // approximation:
+         width:         .7em;
+         height:        .7em;
+         border:        .14em ${cstyle} ${tracecolor};
+         border-top:    .14em ${cstyle} ${top_color};
+         border-bottom: .14em ${cstyle} ${bottom_color};
+         border-left:   .14em ${cstyle} ${left_color};
+         border-right:  .14em ${cstyle} ${right_color};
+       }
+    `;
 
     this.shadowRoot.innerHTML = `
       <style>
@@ -92,47 +139,22 @@ class SpinnerElement extends HTMLElement {
           0%   { transform: rotate(0deg); }
           100% { transform: rotate(-360deg); }
         }
-        button {
-          @supports (width: 1cap) {
-            vertical-align: calc(1em - 1cap);
-          }
-          @supports not (width: 1cap) {
-            vertical-align: .33em;
-          }
-        }
         span {
-          display: inline-block;
-          box-sizing: border-box;
-          margin-left: ${kerning};
-          margin-right: ${kerning};
-          border-radius: 50%;
-          @supports (width: 1cap) {
-            width:         1cap;
-            height:        1cap;
-            border:        ${weight}cap solid ${tracecolor};
-            border-top:    ${weight}cap solid ${top_color};
-            border-bottom: ${weight}cap solid ${bottom_color};
-            border-left:   ${weight}cap solid ${left_color};
-            border-right:  ${weight}cap solid ${right_color};
-          }
-          @supports not (width: 1cap) {
-            // approximation:
-            width:         .7em;
-            height:        .7em;
-            border:        .14em solid ${tracecolor};
-            border-top:    .14em solid ${top_color};
-            border-bottom: .14em solid ${bottom_color};
-            border-left:   .14em solid ${left_color};
-            border-right:  .14em solid ${right_color};
-          }
+          display:      inline-block;
+          padding:      0;
+          box-sizing:   border-box;
+          margin-left:    ${kerning};
+          margin-right:   ${kerning};
+          background-color: ${bgcolor};
+      ${circleBorderSpinnerCss}
           animation: ${animation} ${speed}s linear infinite;
         }
         div {
           display:          inline-block;
-          margin:           0;
           padding:          0;
+          margin:           0;
           color:            ${crsrcolor};
-          ${back_style}
+          ${backStyle}
         }
       </style>
       <div>${prefix}<span></span>${suffix}</div>
@@ -151,39 +173,28 @@ customElements.define('x-spinner', SpinnerElement);
 /* Some basic functions */
 
 function insertSpinner(spinner,target,options) {
-	target	= target.id ? target : document.getElementById(target);
-	target.innerHTML = '';
-	target.appendChild(spinner);
+  const targetElement = (typeof target === 'string') ?
+    document.querySelector(target) : target;
+  targetElement.innerHTML = '';
+  targetElement.appendChild(spinner);
 }
+
 function appendSpinner(spinner,target,options) {
-	target	= target.id ? target : document.getElementById(target);
-	target.appendChild(spinner);
-	spinner.setAttribute('clr', 'red');
+  const targetElement = (typeof target === 'string') ?
+    document.querySelector(target) : target;
+  targetElement.appendChild(spinner);
 }
 
-function removeSpinner(options) {
-	// ...
+function removeSpinner(spinner) {
+  if (spinner instanceof SpinnerElement) {
+    spinner.remove();
+  }
+  else if (typeof spinner === 'string') {
+    const spinnerElement = document.querySelector(spinner);
+    if (spinnerElement instanceof SpinnerElement) {
+      spinnerElement.remove();
+    }
+  } else {
+    console.warn('Invalid argument for removeSpinner. Expected a SpinnerElement instance or a selector string.');
+  }
 }
-
-function SpinnerOptions() {
-	const options	= new String( SpinnerElement.observedAttributes );
-	return options.split(',');
-}
-
-function showSpinnerOptions(target) {
-	target	= target.id ? target : document.getElementById(target);
-	let names = SpinnerOptions() || [];
-	target.innerHTML = names.join(' ');
-}
-
-function SpinnerAppliedOptions(spinner) {
-	const appliedOptions	= spinner.getAttributeNames() || [];
-	return appliedOptions;
-}
-
-function showSpinnerAppliedOptions(spinner,target) {
-	target	= target.id ? target : document.getElementById(target);
- 	let names = SpinnerAppliedOptions(spinner) || [];
-	target.innerHTML = names.join(' ');
-}
-

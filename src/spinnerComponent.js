073552e7d81ab1432a1651fc325cf6641fccea3a
Bruce Van Allen <bva@cruzio.com>
2024-06-08 11:41:09 -0700

    FIxed new attribute precedence and mproved example page
    Protected precedence of newly added attributes.
    Improved examples page; moved specific css from page-wide to sections where needed, to improve clarity of examples

--- src/spinnerComponent.js
+++ src/spinnerComponent.js
@@ -3,36 +3,47 @@ class SpinnerElement extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.observedAttributesSet = new Set(this.constructor.observedAttributes);
+    this.latestAttributes = {};
     this.setAttributes(options);
     this.rendered = false;
   }
 
   static observedAttributes = [
-      'rotor', 'rtr',
-      'rotor-style', 'rstyle',
-      'speed', 'sp', 'direction', 'dir', 'weight', 'wt',
-      'rotor-color', 'rclr', // same as 'color'
-      'background-color', 'bgclr',
-      'trace-color', 'tclr',
-      'back-color', 'bkclr',
-      'prefix', 'pre', 'suffix', 'suf', 'kerning', 'kern',
-      'rotor-status', 'rstatus',
-      'aria-wrap', 'awrap',
-      'aria-role', 'role', 'aria-description',
-      'aria-label', 'aria-busy', 'aria-live',
+      'rotor',              'rtr',
+      'rotor-style',        'rstyle',
+      'speed',              'sp',
+      'direction',          'dir',
+      'weight',             'wt',
+      'rotor-color',        'rclr', 'color',
+      'background-color',   'bgclr',
+      'trace-color',        'tclr',
+      'back-color',         'bkclr',
+      'prefix',             'pre',
+      'suffix',             'suf',
+      'kerning',            'kern',
+      'rotor-status',       'rstatus',
+      'aria-wrap',          'awrap',
+      'aria-role',          'role',
+      'aria-label',
+      'aria-busy',
+      'aria-live',
+      'aria-description',
       // these are standard attributes:
-      'color', 'style', 'name', 'id'
+      'style', 'name', 'id'
   ];
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (this.observedAttributesSet.has(name)) {
+      this.latestAttributes[name] = newValue || '';
       this.render();
     }
   }
 
   setAttributes( options = {} ) {
+    this.latestAttributes = {};
     Object.entries(options).forEach(([key, value]) => {
       if (this.observedAttributesSet.has(key)) {
+        this.latestAttributes[key] = value;
         this.setAttribute(key, value);
       } else {
         console.warn(`Ignoring unknown attribute "${key}" for <x-spinner>`);
@@ -67,7 +78,7 @@ class SpinnerElement extends HTMLElement {
       ariaBusy        : alts.ariaBusy        || 'true',
       ariaAtomic      : alts.ariaAtomic      || 'true',
       ariaRelevant    : alts.ariaRelevant    || 'text',
-      ariaLabel       : alts.ariaLabel       || 'Application Status',
+      ariaLabel       : alts.ariaLabel       || 'spinner-prefix spinner-suffix',
       ariaLabelledBy  : alts.ariaLabelledBy  || 'spinner-prefix spinner-suffix',
       ariaDescription : alts.ariaDescription || ''
     };
@@ -129,38 +140,40 @@ class SpinnerElement extends HTMLElement {
   render() {
     const defaults     = this.setDefaults( {  } ); // { rotor: '1010' }
 
-    const spAttributes = Array.from(this.attributes).reduce((obj, attr) => {
+    let spAttributes = Array.from(this.attributes).reduce((obj, attr) => {
       if (this.observedAttributesSet.has(attr.name)) {
         obj[attr.name] = attr.value;
       }
       return obj;
     }, {});
+    let newAttributes = this.latestAttributes;
 
-    const rotor      = spAttributes['rotor']           ||= spAttributes['rtr']    || defaults.rotor;
-    const rtrcolor   = spAttributes['rotor-color']     ||= spAttributes['rclr']   ||= spAttributes['color'] || defaults.rtrcolor;
-    const tracecolor = spAttributes['trace-color']     ||= spAttributes['tclr']   || defaults.tracecolor;
-    const bkcolor    = spAttributes['back-color']      ||= spAttributes['bkclr']  || defaults.bkcolor;
-    const bgcolor    = spAttributes['background-color']||= spAttributes['bgclr']  || defaults.bgcolor;
-    const speed      = spAttributes['speed']           ||= spAttributes['sp']     || defaults.speed;
-    const kerning    = spAttributes['kerning']         ||= spAttributes['kern']   || defaults.kerning;
-    const prefix     = spAttributes['prefix']          ||= spAttributes['pre']    || defaults.prefix;
-    const suffix     = spAttributes['suffix']          ||= spAttributes['suf']    || defaults.suffix;
-    const rtrstyle   = spAttributes['rotor-style']     ||= spAttributes['rstyle'] || defaults.rtrstyle;
-    const rstatus    = spAttributes['rotor-status']    ||= spAttributes['rstatus']|| defaults.rstatus;
-    const direction  = spAttributes['direction']       ||= spAttributes['dir']    || defaults.direction;
-    const weight     = spAttributes['weight']          ||= spAttributes['wt']     || defaults.weight;
+    const rtrcolor   = newAttributes['rotor-color']      || newAttributes['rclr']    || newAttributes['color']
+                         || spAttributes['rotor-color']  || spAttributes['rclr']     || spAttributes['color']            || defaults.rtrcolor;
+    const rotor      = newAttributes['rotor']            || newAttributes['rtr']     || spAttributes['rotor']            || spAttributes['rtr']     || defaults.rotor;
+    const tracecolor = newAttributes['trace-color']      || newAttributes['tclr']    || spAttributes['trace-color']      || spAttributes['tclr']    || defaults.tracecolor;
+    const bkcolor    = newAttributes['back-color']       || newAttributes['bkclr']   || spAttributes['back-color']       || spAttributes['bkclr']   || defaults.bkcolor;
+    const bgcolor    = newAttributes['background-color'] || newAttributes['bgclr']   || spAttributes['background-color'] || spAttributes['bgclr']   || defaults.bgcolor;
+    const speed      = newAttributes['speed']            || newAttributes['sp']      || spAttributes['speed']            || spAttributes['sp']      || defaults.speed;
+    const kerning    = newAttributes['kerning']          || newAttributes['kern']    || spAttributes['kerning']          || spAttributes['kern']    || defaults.kerning;
+    const prefix     = newAttributes['prefix']           || newAttributes['pre']     || spAttributes['prefix']           || spAttributes['pre']     || defaults.prefix;
+    const suffix     = newAttributes['suffix']           || newAttributes['suf']     || spAttributes['suffix']           || spAttributes['suf']     || defaults.suffix;
+    const rtrstyle   = newAttributes['rotor-style']      || newAttributes['rstyle']  || spAttributes['rotor-style']      || spAttributes['rstyle']  || defaults.rtrstyle;
+    const rstatus    = newAttributes['rotor-status']     || newAttributes['rstatus'] || spAttributes['rotor-status']     || spAttributes['rstatus'] || defaults.rstatus;
+    const direction  = newAttributes['direction']        || newAttributes['dir']     || spAttributes['direction']        || spAttributes['dir']     || defaults.direction;
+    const weight     = newAttributes['weight']           || newAttributes['wt']      || spAttributes['weight']           || spAttributes['wt']      || defaults.weight;
 
-    const awrap           = spAttributes['aria-wrap']        ||= spAttributes['awrap']   || defaults.awrap;
-    const ariaRole        = spAttributes['aria-role']        ||= spAttributes['role']    || defaults.ariaRole;
-    const ariaLabel       = spAttributes['aria-label']       || defaults.ariaLabel;
-    const ariaBusy        = spAttributes['aria-busy']        || defaults.ariaBusy;
-    const ariaLive        = spAttributes['aria-live']        || defaults.ariaLive;
-    const ariaDescription = spAttributes['aria-description'] || defaults.ariaDescription;
+    const awrap           = newAttributes['aria-wrap']        || newAttributes['awrap']           || spAttributes['aria-wrap'] || spAttributes['awrap'] || defaults.awrap;
+    const ariaRole        = newAttributes['aria-role']        || newAttributes['role']            || spAttributes['aria-role'] || spAttributes['role']  || defaults.ariaRole;
+    const ariaLabel       = newAttributes['aria-label']       || spAttributes['aria-label']       || defaults.ariaLabel;
+    const ariaBusy        = newAttributes['aria-busy']        || spAttributes['aria-busy']        || defaults.ariaBusy;
+    const ariaLive        = newAttributes['aria-live']        || spAttributes['aria-live']        || defaults.ariaLive;
+    const ariaDescription = newAttributes['aria-description'] || spAttributes['aria-description'] || defaults.ariaDescription;
     const ariaAtomic      = defaults.ariaAtomic;
     const ariaRelevant    = defaults.ariaRelevant;
     const ariaLabelledBy  = defaults.ariaLabelledBy;
 
-    // using weight
+    // using weight to get the rotor weight
     let rWeight;
     if (weight <= .5 && weight > 0) {
       rWeight  = weight;
@@ -172,14 +185,14 @@ class SpinnerElement extends HTMLElement {
       rWeight  = defaults.weight;
     }
 
-    // using rotor
+    // using rotor to get the border coloring
     const rtr_pat     = rotor.split('');
     const top_color   = rtr_pat[0] == 1 ? rtrcolor : tracecolor;
     const left_color  = rtr_pat[1] == 1 ? rtrcolor : tracecolor;
     const bottom_color= rtr_pat[2] == 1 ? rtrcolor : tracecolor;
     const right_color = rtr_pat[3] == 1 ? rtrcolor : tracecolor;
 
-    // using direction
+    // using direction to get clockwise/counter-clockwise
     const rtFrom      = '0deg';
     const rtTo        = direction === 'cw' ? '360deg' : '-360deg';
 
